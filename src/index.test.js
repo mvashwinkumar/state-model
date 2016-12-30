4522e719b3b70b528525772a00792ae5e8e1c3d9
mvashwinkumar <mvashwinkumar@gmail.com>
2016-12-30 20:58:16 +0800

    Update project structure layout

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('should run mocha', function() {
+    it('should run', function() {
+        expect(true).to.be.true
+    })
+})
