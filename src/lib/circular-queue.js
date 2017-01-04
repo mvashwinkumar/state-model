ac5bc5176ecb6c6f2b2aeee280f8663f6b63bbc6
mvashwinkumar <mvashwinkumar@gmail.com>
2017-01-04 22:37:51 +0800

    feat(module): Circular Queue Data structure methods implementation
    Implement enqueue, dequeue and find operations in data structure

--- src/lib/circular-queue.js
+++ src/lib/circular-queue.js
@@ -11,12 +11,22 @@ const OPTIONS = {
     },
     COLLTYPE : {
         OPT : 'collectionType',
-        OBJECT : 'Object'
+        OBJECT : Object
     },
     OVERFLOW : {
         OPT : 'overflow',
-        SILENT : 'silent'
+        SILENT : 'silent',
+        THROW : 'throw'
     }
+};
+
+const ERR = {
+    NOT_INSTANCE : 'NOT_INSTANCE : This object is not an instance of collection type',
+    EMPTY : 'EMPTY : This queue is empty',
+    FULL : 'FULL : This queue at full capacity',
+    INVALID_ARG : 'INVALID_ARG : Invalid Argument(s)',
+    KEY_NOT_FOUND : 'KEY_NOT_FOUND : Requested key not found in queue',
+    REPLACE_OVERFLOW : 'REPLACE_OVERFLOW : More objects to replace than found from startKey'
 };
 
 let defaultOpts = {};
@@ -25,23 +35,59 @@ defaultOpts[OPTIONS.KEY.OPT] = OPTIONS.KEY.ID;
 defaultOpts[OPTIONS.COLLTYPE.OPT] = OPTIONS.COLLTYPE.OBJECT;
 defaultOpts[OPTIONS.OVERFLOW.OPT] = OPTIONS.OVERFLOW.SILENT;
 
-// function _next() {}
+function _next() {
+    let afterLastPtr = _resolvePtr(this.lastPtr + 1, this.opts[OPTIONS.MAXSIZE.OPT]);
+    if(afterLastPtr == (this.topPtr)) {
+        return false;
+    } else {
+        this.lastPtr = afterLastPtr;
+        return true;
+    }
+}
 
-// function _prev() {}
+function _prev() {
+    let beforeTopPtr = _resolvePtr(this.topPtr - 1, this.opts[OPTIONS.MAXSIZE.OPT]);
+    if(beforeTopPtr == (this.lastPtr)) {
+        return false;
+    } else {
+        this.topPtr = beforeTopPtr;
+        return true;
+    }
+}
 
-// function _resolvePtr(ptr, maxSize) {}
+function _resolvePtr(ptr, maxSize) {
+    if(ptr >= 0) {
+        return ptr % maxSize;
+    } else {
+        return (maxSize + ptr);
+    }
+}
 
-// function _isInstance(obj) {}
+function _isInstance(obj) {
+    return obj instanceof this.opts[OPTIONS.COLLTYPE.OPT];
+}
 
-// function _handleOverflow(obj) {}
+function _handleOverflow(obj) {
+    if(this.opts[OPTIONS.OVERFLOW.OPT] === OPTIONS.OVERFLOW.SILENT) {
+        let maxSize = this.opts[OPTIONS.MAXSIZE.OPT];
+        this.lastPtr = _resolvePtr(this.lastPtr + 1, maxSize);
+        this.topPtr = _resolvePtr(this.topPtr - 1, maxSize);
+        this.data[this.lastPtr] = _.cloneDeep(obj);
+        return true;
+    } else {
+        throw new Error(ERR.FULL);
+    }
+}
 
-// function _handleUnderflow() {}
+function _handleUnderflow() {
+    throw new Error(ERR.EMPTY);
+}
 
 export class CircularQueue {
     // options - max data size : non-negative number || Infinity
     // options - key : class member to search with peek || id
     // options - collectionType : allowed object instance of a class || Object
-    // options - overflow : behaviour on overflow is throw err, override last  || throw err
+    // options - overflow : behaviour on overflow is throw err, override top  || throw err
     constructor(list, opts) {
         this.data = _.cloneDeep(list);
         this.opts = _.extend(defaultOpts, opts);
@@ -49,22 +95,37 @@ export class CircularQueue {
         this.topPtr = 0;
     }
 
-    push(obj) {
-        return false;
+    enqueue(obj) {
+        if(!_isInstance.call(this, obj)) {
+            throw new Error(ERR.NOT_INSTANCE);
+        }
+        if(_next.call(this)) {
+            this.data[this.lastPtr] = _.cloneDeep(obj);
+            return true;
+        } else {
+            return _handleOverflow.call(this, obj);
+        }
     }
 
-    pop() {
-        return false;
+    dequeue() {
+        let top = this.data[this.topPtr];
+        this.data[this.topPtr] = null;
+        if(_prev.call(this)) {
+            return top;
+        } else {
+            return _handleUnderflow.call(this);
+        }
     }
 
     find(key) {
-        return false;
+        // returns the reference to the data instance with matching key as defined in options
+        return _.find(this.data, (obj) => key === obj[this.opts[OPTIONS.KEY.OPT]]);
     }
 
-    replace(startKey, numObjs, replaceObjs) {
-        // replaces the data with replaceObjs in collection from startKey until numObjs
-        // if size different, then resize collection elements
-        return false;
-    }
+    // replace() {
+    //     // replaces the data with replaceObjs in collection from startKey until numObjs
+    //     // if size different, then resize collection elements
+    //     return false;
+    // }
 
 }
--- src/lib/circular-queue.test.js
+++ src/lib/circular-queue.test.js
@@ -12,9 +12,75 @@ describe('Circular Queue Module', function() {
         opts.maxSize = 10;
         let cq = new CQ(list, opts);
         expect(cq.opts.maxSize).equal(10);
-        expect(cq.push()).exist;
-        expect(cq.pop()).exist;
-        expect(cq.find()).exist;
-        expect(cq.replace()).exist;
+        expect(cq.enqueue).be.function;
+        expect(cq.dequeue).be.function;
+        expect(cq.find).be.function;
+        // expect(cq.replace()).be.function;
+    })
+    it('should push, find and pop object', function() {
+        let list = [];
+        list.push({id:1, name:'one'});
+        list.push({id:2, name:'two'});
+        list.push({id:3, name:'three'});
+        let opts = {
+            maxSize : 10,
+            overflow : 'silent'
+        };
+
+        let cq = new CQ(list, opts);
+
+        cq.enqueue({id:4, name:'four'});
+        expect(cq.find(4)).to.have.any.keys('name');
+
+        let firstDequeue = cq.dequeue();
+        expect(firstDequeue).deep.equal(list[0]);
+    })
+    it('should throw error when push invalid object', function() {
+        let Car = (id, name) => {
+            this.id = id;
+            this.name = name;
+        };
+        let opts = {
+            maxSize : 10,
+            collectionType : Car
+        };
+        let carList = [];
+        carList.push(new Car(1,'Car 1'));
+        carList.push(new Car(2,'Car 2'));
+        let cq = new CQ(carList, opts);
+
+        let errMsg = "";
+        try {
+            cq.enqueue({id:4, name:'four'});
+        } catch (e) {
+            errMsg = e.message;
+        } finally {
+            expect(errMsg).to.equal('NOT_INSTANCE : This object is not an instance of collection type');
+        }        
+    })
+    it('should throw error when push more than capacity', function() {
+        let Car = (id, name) => {
+            this.id = id;
+            this.name = name;
+        };
+        let opts = {
+            maxSize : 2,
+            collectionType : Car,
+            overflow : 'throw'
+        };
+        let carList = [];
+        carList.push(new Car(1,'Car 1'));
+        carList.push(new Car(2,'Car 2'));
+        let cq = new CQ(carList, opts);
+
+        let errMsg = "";
+        try {
+            cq.enqueue(new Car(3,'Car 3'));
+        } catch (e) {
+            errMsg = e.message;
+        } finally {
+            expect(errMsg).to.equal('FULL : This queue at full capacity');
+        }
+        
     })
 })
